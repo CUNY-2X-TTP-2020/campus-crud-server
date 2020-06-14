bb47e82a3c5a52652780b6e3bce22c7f6d9a219c
RickyC0626 <rickychon99@gmail.com>
2020-06-13 23:51:29 -0400

    Add route to serve up a single student based on their id

--- routes/students.js
+++ routes/students.js
@@ -13,7 +13,7 @@ const { Student, Campus } = require("../database/models");
  */
 router.get("/", async (req, res, next) =>
 {
-    // Try to get campuses object from database
+    // Try to get students object from database
     try
     {
         // Students will be the result of Student.findAll promise
@@ -26,6 +26,31 @@ router.get("/", async (req, res, next) =>
     {
         // If there is an error, it will be passed via the next parameter to the 
         // error handler middleware
+        next(err);
+    }
+});
+
+/**
+ * GET a single student based on their id
+ * /api/students/:id
+ * /api/students/10 would respond with a student with id 10
+ */
+router.get("/:id", async (req, res, next) =>
+{
+    // Get the id from params
+    const { id } = req.params;
+
+    // Query the database for a student with matching id
+    try
+    {
+        // If successful
+        const student = await Student.findByPk(id, { include: Campus });
+
+        // Send back the student as a response
+        res.status(200).json(student);
+    }
+    catch(err)
+    {
         next(err);
     }
 });
